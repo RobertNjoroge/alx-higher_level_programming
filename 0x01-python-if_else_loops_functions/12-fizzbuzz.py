a44d01ef876c0ee16883b9363f17a0abcfbea01d
Robert Njoroge <mainarobert961@gmail.com>
2022-11-29 06:22:14 -0800

    alphabets with for loop

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,11 +3,11 @@
 def fizzbuzz():
     for i in range(1, 101):
         if ((i % 3 == 0) and (i % 5 != 0)):
-            print(" FIZZ", end=" ")
+            print(" Fizz", end=" ")
         elif ((i % 5 == 0) and (i % 3 != 0)):
-            print("  BUZZ", end=" ")
+            print("  Buzz", end=" ")
         elif ((i % 3 == 0) and (i % 5 == 0)):
-            print(" FIZZBUZZ", end=" ")
+            print(" FizzBuzz", end=" ")
         elif i == 1:
             print("{}".format(i), end=" ")
         else:
