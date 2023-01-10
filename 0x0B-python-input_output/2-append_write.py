e5cfea91119e84db18b2f0e002eedd8f3aeca6a6
Robert Njoroge <97443055+RobertNjoroge@users.noreply.github.com>
2023-01-10 13:03:38 +0300

    f.write(append)

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -8,9 +8,8 @@ def append_write(filename="", text=""):
     Args:
        filename: represents the name of file.
        text: represents the text to be appended.
-
     Returns:
-         returns the number of characters added
+       returns the number of characters added
     """
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
